b81393c5f6a0b3bb872f4ba41df635cdeec25d4d
Matt Morgan <m@platypi.io>
2021-08-11 16:53:30 -0400

    adding options to avaudio session

--- src/ios/APPBackgroundMode.m
+++ src/ios/APPBackgroundMode.m
@@ -194,7 +194,7 @@ NSString* const kAPPBackgroundEventDeactivate = @"deactivate";
 
     // Play music even in background and dont stop playing music
     // even another app starts playing sound
-    [session setCategory:AVAudioSessionCategoryPlayback
+    [session setCategory:AVAudioSessionCategoryPlayback withOptions:AVAudioSessionCategoryOptionMixWithOthers
                    error:NULL];
 
     // Active the audio session
